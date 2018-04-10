abb13b02a2455ab43436705a816e65aa3f357136
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-10 18:27:34 +0000

    automatically backed up by learn

--- spec/helpers/authors_helper_spec.rb
+++ spec/helpers/authors_helper_spec.rb
@@ -1,9 +1,2 @@
 require 'rails_helper'
 #Created my own test
-describe AuthorsHelper do
-  describe "string concat" do
-    it "concats two strings with spaces" do
-      expect(helper.concat_strings("this","that")).to eq("this that")
-    end
-  end
-end
